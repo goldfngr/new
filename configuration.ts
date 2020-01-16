c3f4945624dcc552c0ec07a83d3b2598e21c41fe
goldfngr <gbager@gmail.com>
2020-01-16 12:00:09 -0500

    adding all Dist files

---
+++ configuration.ts
@@ -0,0 +1,8 @@
+export const configuration = {
+    header: {
+        heading: 'My Angular website demo',
+        headingtext: "Namari is a free landing page template you can use for your projects. It is free to use for your personal and commercial projects so enjoy\!",
+        buttontext: "Button. Click Me >",
+        buttonlink: '\home'
+    },
+  };
